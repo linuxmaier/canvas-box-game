d3fa0dec413238889fef4a75b62fa23cad323666
linuxmaier <andrew.eric.maier@gmail.com>
2013-09-22 15:32:05 -0500

    changed comment formating

--- shapes.js
+++ shapes.js
@@ -1,7 +1,10 @@
+
+function Shape(xloc, yloc) {
+
 /*super class that contains all shapes in game. includes location
 and velocity info, since all objects will have location and some
 may move. Also contains fill style and border defaults*/
-function Shape(xloc, yloc) {
+
 	this.x = xloc;
 	this.y = yloc;
 	this.fillStyle = "#FFFFFF";
@@ -9,5 +12,3 @@ function Shape(xloc, yloc) {
 	this.xVelocity = 0;
 	this.yVelocity = 0;
 }
-
-
