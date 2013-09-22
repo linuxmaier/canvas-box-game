090d1db21961888386420ffc57839002ea3f03be
linuxmaier <andrew.eric.maier@gmail.com>
2013-09-22 15:30:26 -0500

    added comments to shapes superclass

--- shapes.js
+++ shapes.js
@@ -1,7 +1,11 @@
+/*super class that contains all shapes in game. includes location
+and velocity info, since all objects will have location and some
+may move. Also contains fill style and border defaults*/
 function Shape(xloc, yloc) {
 	this.x = xloc;
 	this.y = yloc;
 	this.fillStyle = "#FFFFFF";
+	this.borderWidth = 1;
 	this.xVelocity = 0;
 	this.yVelocity = 0;
 }
