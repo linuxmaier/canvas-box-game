3cf650c6e166ec77197eb5912294de1af46cc104
linuxmaier <andrew.eric.maier@gmail.com>
2013-10-19 15:47:37 -0500

    added physics.js

---
+++ physics.js
@@ -0,0 +1,14 @@
+//contains an object which controls the physics constants of the game.
+
+var gamePhysics = {
+	//multiplier applie to velocity to slow movement
+	surfaceFric: .95,
+	//rate of increase for moving shapes
+	acceleration: 20,
+	//top limit on speed for objects to move
+	maxSpeed: 100,
+	//energy absorbtion by walls
+	eAbsorb: .75
+};
+
+
