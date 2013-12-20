19dfc2aab6ca21064ec2705ca254f78ded10e551
linuxmaier <andrew.eric.maier@gmail.com>
2013-12-19 21:00:57 -0600

    added shapeInfo object

--- player.js
+++ player.js
@@ -2,4 +2,19 @@ function Player(name, shapeinfo, physics, keys) {
 	this.score = 0;
 	this.name = name;
 	this.shape = new Rectangle(shapeinfo.x, shapeinfo.y, shapeinfo.width, shapeinfo.height, shapeinfo.fillStyle, physics, keys, shapeinfo.keymap);
+	this.fricDebuff = 1;
+	this.acceDebuff = 1;
+}
+
+Player.debuff = new function(type) {
+	
+}
+
+function shapeInfo (x, y, width, height, color, keymap) {
+	this.x = x;
+	this.y = y;
+	this.width = width;
+	this.height = height;
+	this.fillStyle = color;
+	this.keymap = keymap;
 }
